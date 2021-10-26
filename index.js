50e957bedbac3909e222d59f5cf5ce5f839f3aa0
Ricardo <rcardo.peralta@gmail.com>
2021-10-26 17:09:01 -0500

    allow cross origin

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const express = require('express');
 
 const app = express();
 
+let allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Headers', "*");
+  next();
+}
+app.use(allowCrossDomain);
+
 app.use(express.json());
 app.use(router);
 
